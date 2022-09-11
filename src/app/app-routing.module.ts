0b0fde3a44bda5a0c037e0c2da9804f42afec75d
Yudi Haryasa <i.putu.yudi.haryasa@gdplabs.id>
2022-09-11 17:04:58 +0800

    feat: add basic page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BasicComponent } from './basic/basic.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'basic',
+    pathMatch: 'full'
+  },
+  {
+    path: 'basic',
+    component: BasicComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
